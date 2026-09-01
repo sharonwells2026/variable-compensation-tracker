a3dd79a67218958a6d0c7e6d1c170536bbe45fc0
Sharon Wells <sharonwells@engagifii.com>
2026-09-01 15:53:35 -0400

    Fix required post-approval workflow readiness

--- app/employee-administration/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./employee-administration.css";
-
-export default function EmployeeAdministrationLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="employee-admin-route-shell">{children}</div>;
-}
--- app/user-administration/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./user-administration.css";
-
-export default function UserAdministrationLayout({children}:{children:React.ReactNode}){
-  return <div className="user-admin-route-shell">{children}</div>;
-}
